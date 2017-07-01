b99a4fe4c1741c68163e2ba395e8ab26c4f40a5a
tdd <tdd_style@163.com>
2017-07-01 18:10:08 +0800

    add router.js

--- htdocs/boot/index.js
+++ htdocs/boot/index.js
@@ -1,10 +1,14 @@
 ﻿var http = require('http');
+var router = require("../common/router");
+console.log(router);
+
 var port = 1224;
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text-plain' });
     response.end('Hello World\n');
 	console.log(request);
 	console.log(response);
+    router.GET(request, response);
 }).listen(1224);
 console.log("server start ok");
 console.log("listen port: ",port);
