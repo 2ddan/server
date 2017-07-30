e52dff619876c3a9d3c158a1b579cdd70f8a1a70
唐丹丹 <tangdandan@tangdandandeMacBook-Pro.local>
2017-07-30 19:34:03 +0800

    modify index.js

--- htdocs/boot/index.js
+++ htdocs/boot/index.js
@@ -1,14 +1,33 @@
-﻿var http = require('http');
-var router = require("../common/router");
+﻿/**
+ * @description Module dependencies.
+ * @private 
+ */
+
+/***** Module dependencies *****/
+//node modules
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+//private modules
+const router = require("../common/router");
 console.log(router);
 
-var port = 1224;
-http.createServer(function (request, response) {
+/***** Module variables *****/
+
+const port = 1224,
+    serv;
+
+/***** Module exports *****/
+
+/***** local running ******/
+
+serv = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text-plain' });
     response.end('Hello World\n');
 	console.log(request);
 	console.log(response);
     router.GET(request, response);
-}).listen(1224);
+});
+serv.listen(port);
 console.log("server start ok");
 console.log("listen port: ",port);
