e5816b7aa4232cecead546df13af689420188ead
唐丹丹 <tangdandan@tangdandandeMacBook-Pro.local>
2018-10-13 08:28:08 +0800

    rewrite

---
+++ htdocs/boot/server.js
@@ -0,0 +1,33 @@
+﻿/**
+ * @description Module dependencies.
+ * @private 
+ */
+
+/***** Module dependencies *****/
+
+//node modules
+const url = require('url');
+const path = require("path");
+const fs = require('fs');
+
+/***** server *****/
+exports.NI = (function(){
+    const _exports = {},
+        http = require('http');
+
+    /**
+     * @description start server
+     */
+    _exports.run = ()=>{
+        
+    }
+
+
+    return _exports;
+})();
+
+/***** Module exports *****/
+
+/***** Module variables *****/
+
+/***** local running ******/
